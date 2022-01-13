cce5331889bdae062fca5182c4c45f9451dad1f3
Isaac Yee <48425750+IY314@users.noreply.github.com>
2022-01-12 21:53:49 -0800

    Add create-c-proj tool

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	@g++ src/main.cc -Ilib/argparse/include -Wall -o bin/create-c-proj -std=gnu++17
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <string>
+#include <sstream>
 #include "argparse/argparse.hpp"
 
 #define OPTIONS ""
@@ -12,5 +13,20 @@ struct ProgArgs : public argparse::Args {
 
 int main(int argc, char *argv[]) {
     ProgArgs args = argparse::parse<ProgArgs>(argc, argv);
-    args.print();
+    std::ostringstream path_stream;
+    path_stream << args.project_path << "/" << args.project_name;
+    std::string path = path_stream.str();
+    std::ostringstream mkdir_root_cmd;
+    std::string subdirs[] = {{"src"}, {"bin"}, {"lib"}, {"inc"}};
+    mkdir_root_cmd << "mkdir -p " << path;
+    for (std::string dir : subdirs) {
+        std::ostringstream mkdir_cmd;
+        mkdir_cmd << mkdir_root_cmd.str() << "/" << dir;
+        std::system(mkdir_cmd.str().c_str());
+    }
+    std::ostringstream cd_cmd;
+    cd_cmd << "cd " << path;
+    std::system(cd_cmd.str().c_str());
+    if (args.git)
+        std::system("git init");
 }
